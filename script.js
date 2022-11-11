1210e7c454d2785f7faed3f44671a784656ae8ee
Trever O <toveson9522@gmail.com>
2022-11-10 19:50:59 -0700

    making password now

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-// Assignment Code
-// GIVEN I need a new, secure password
 var generateBtn = document.querySelector("#generate")
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password")
-
+  const password = []
   const passwordLength = prompt("How many characters do you want your password to be?")
-  const lowercase = confirm("Do you want to include lowercase characters?")
-  const uppercase = confirm("Do you want to include uppercase characters?")
-  const numeric = confirm("Do you want to include numbers characters?")
-  const specialCharacters = confirm("Do you want to include special characters characters?")
+  
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log("password correct length")
+    const lowercase = confirm("Do you want to include lowercase characters?")
+    const uppercase = confirm("Do you want to include uppercase characters?")
+    const numeric = confirm("Do you want to include numbers characters?")
+    const specialCharacters = confirm("Do you want to include special characters characters?")
 
-  passwordText.value = password
 
+    for (let i = 0; i < passwordLength; i++) {
+      if(lowercase) password.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97))
+      if(uppercase) password.push(String.fromCharCode(Math.floor(Math.random() * 26) + 65))
+      if(numeric) password.push(String.fromCharCode(Math.floor(Math.random() * 10) + 48))
+      if(specialCharacters) {
+        const symbols = '!@#$%^&*(){}[]=<>/,.'
+        password.push(symbols[Math.floor(Math.random() * symbols.length)])
+      }
+    }
+
+    let i = password.length, j, temp;
+    while (--i > 0) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = password[j];
+        password[j] = password[i];
+        password[i] = temp;
+    };
+    
+  } else {
+    alert("password must be at least 8 characters and not more than 128 characters")
+    writePassword()
+  }
+  
+  passwordText.value = password.splice(0, passwordLength).join('')
 }
 
 // Add event listener to generate button

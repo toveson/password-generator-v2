cab34208cf7c3426e7f0a508f7a2facb45792845
Trever O <toveson9522@gmail.com>
2022-11-10 18:47:54 -0700

    added variables for user input

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// GIVEN I need a new, secure password
+var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password")
 
+  const passwordLength = prompt("How many characters do you want your password to be?")
+  const lowercase = confirm("Do you want to include lowercase characters?")
+  const uppercase = confirm("Do you want to include uppercase characters?")
+  const numeric = confirm("Do you want to include numbers characters?")
+  const specialCharacters = confirm("Do you want to include special characters characters?")
 
-  // GIVEN I need a new, secure password
-  // WHEN I click the button to generate a password
-  // THEN I am presented with a series of prompts for password criteria
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // WHEN asked for character types to include in the password
-  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-  // WHEN I answer each prompt
-  // THEN my input should be validated and at least one character type should be selected
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
-  // WHEN the password is generated
-  // THEN the password is either displayed in an alert or written to the page
-
-  passwordText.value = password;
+  passwordText.value = password
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
